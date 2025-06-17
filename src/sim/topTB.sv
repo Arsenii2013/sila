66e56453eb9406d0d09afde3564a4f49c5c3a1d6
Arsenii2013 <a.nepein@g.nsu.ru>
2025-06-17 13:22:14 +0700

    Add evr and evg to top module

--- src/sim/topTB.sv
+++ src/sim/topTB.sv
@@ -4,47 +4,78 @@
 module topTB(
 
     );
+    localparam PROPAGATION_DELAY = 1234.56ns;
 
     logic [3:0] led;
-    logic     sysclk;
+    logic     sysclk [2];
 
     sys_clk_gen
     #(
-        .halfcycle (5000), // 5000 ps = 100 MHz
+        .halfcycle (2500), // 2500 ps = 200 MHz on board system clock
         .offset    (0)
-    ) CLK_GEN (
-        .sys_clk (sysclk)
+    ) CLK_GEN1 (
+        .sys_clk (sysclk[0])
+    );
+    sys_clk_gen
+    #(
+        .halfcycle (2500.01), // 2500 ps = 200 MHz on board system clock
+        .offset    (0)
+    ) CLK_GEN2 (
+        .sys_clk (sysclk[1])
     );
 
-    logic     REFCLK_SFP;
+
+    logic     REFCLK_SFP[2];
 
     sys_clk_gen
     #(
         .halfcycle (4000), // 4000 ps = 125 MHz
         .offset    (0)
-    ) REFCLK_SFP_gen (
-        .sys_clk (REFCLK_SFP)
+    ) REFCLK_SFP_gen1 (
+        .sys_clk (REFCLK_SFP[0])
+    );
+    sys_clk_gen
+    #(
+        .halfcycle (4000.01), // 4000 ps = 125 MHz
+        .offset    (0)
+    ) REFCLK_SFP_gen2 (
+        .sys_clk (REFCLK_SFP[1])
     );
 
-    logic sfp_rx_n [4];
-    logic sfp_rx_p [4];
-    logic sfp_tx_n [4];
-    logic sfp_tx_p [4];
+    logic sfp_rx_n [2][4];
+    logic sfp_rx_p [2][4];
+    logic sfp_tx_n [2][4];
+    logic sfp_tx_p [2][4];
 
-    assign sfp_rx_n = sfp_tx_n;
-    assign sfp_rx_p = sfp_tx_p;
+    always @(sfp_tx_p[0][0])    sfp_rx_p[1][2]    <= #(PROPAGATION_DELAY) sfp_tx_p[0][0];
+    always @(sfp_tx_n[0][0])    sfp_rx_n[1][2]    <= #(PROPAGATION_DELAY) sfp_tx_n[0][0];
+    always @(sfp_tx_p[1][2])    sfp_rx_p[0][0]    <= #(PROPAGATION_DELAY) sfp_tx_p[1][2];
+    always @(sfp_tx_n[1][2])    sfp_rx_n[0][0]    <= #(PROPAGATION_DELAY) sfp_tx_n[1][2];
 
-    top DUT(
-        .sysclk_n(~sysclk),
-        .sysclk_p(sysclk),
-        .REFCLK_SFP_n(~REFCLK_SFP),
-        .REFCLK_SFP_p(REFCLK_SFP),
+    top DUT1(
+        .sysclk_n(~sysclk[0]),
+        .sysclk_p(sysclk[0]),
+        .REFCLK_SFP_n(~REFCLK_SFP[0]),
+        .REFCLK_SFP_p(REFCLK_SFP[0]),
 
-        .sfp_rx_n(sfp_rx_n),
-        .sfp_rx_p(sfp_rx_p),
-        .sfp_tx_n(sfp_tx_n),
-        .sfp_tx_p(sfp_tx_p),
+        .sfp_rx_n(sfp_rx_n[0]),
+        .sfp_rx_p(sfp_rx_p[0]),
+        .sfp_tx_n(sfp_tx_n[0]),
+        .sfp_tx_p(sfp_tx_p[0]),
         .led(led)
+    );
+
+    top DUT2(
+        .sysclk_n(~sysclk[1]),
+        .sysclk_p(sysclk[1]),
+        .REFCLK_SFP_n(~REFCLK_SFP[1]),
+        .REFCLK_SFP_p(REFCLK_SFP[1]),
+
+        .sfp_rx_n(sfp_rx_n[1]),
+        .sfp_rx_p(sfp_rx_p[1]),
+        .sfp_tx_n(sfp_tx_n[1]),
+        .sfp_tx_p(sfp_tx_p[1]),
+        .led()
     );
 
 initial begin
