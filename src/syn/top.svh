672b3b8b3326ad8cde5e7e4e9497d3977f51485d
Arsenii2013 <a.nepein@g.nsu.ru>
2025-08-31 15:30:48 +0700

    Moved general axi params to separete fine

--- src/syn/top.svh
+++ src/syn/top.svh
@@ -5,4 +5,27 @@
 //      Common
 localparam CLK_PRD         = 8;
 
+package axi_params;
+
+import rdl_axi_params_pkg::*;
+
+localparam rdl_axi_params__gp0_params_e     GP0_ADDR_W_EVAL   = rdl_axi_params__gp0_params__GP0_ADDR_W;
+localparam rdl_axi_params__gp0_params_e     GP0_DATA_W_EVAL   = rdl_axi_params__gp0_params__GP0_DATA_W;
+localparam rdl_axi_params__mmr_params_e     MMR_ADDR_W_EVAL   = rdl_axi_params__mmr_params__MMR_BLOCK_ADDR_W;
+localparam rdl_axi_params__mmr_params_e     MMR_DATA_W_EVAL   = rdl_axi_params__mmr_params__MMR_BLOCK_DATA_W;
+localparam rdl_axi_params__mmr_dev_params_e MMR_DEV_CNT2_EVAL = rdl_axi_params__mmr_dev_params__MMR_DEV_CNT;
+
+localparam GP0_ADDR_W     = unsigned'(GP0_ADDR_W_EVAL);
+localparam GP0_DATA_W     = unsigned'(GP0_DATA_W_EVAL);
+localparam MMR_ADDR_W     = unsigned'(MMR_ADDR_W_EVAL);
+localparam MMR_DATA_W     = unsigned'(MMR_DATA_W_EVAL);
+localparam MMR_DEV_CNT2   = unsigned'(MMR_DEV_CNT2_EVAL);
+
+typedef logic [GP0_ADDR_W-1:0] gp0_addr_t;
+typedef logic [GP0_DATA_W-1:0] gp0_data_t;
+typedef logic [MMR_ADDR_W-1:0] mmr_addr_t;
+typedef logic [MMR_DATA_W-1:0] mmr_data_t;
+
+endpackage
+
 `endif //__TOP_SVH__
--- src/syn/topEVR.svh
+++ src/syn/topEVR.svh
@@ -2,28 +2,6 @@
 `ifndef __TOP_EVR__
 `define __TOP_EVR__
 
-package axi_params;
-
-import EVR_reg_map_pkg::*;
-
-localparam EVR_reg_map__gp0_params_e        GP0_ADDR_W_EVAL   = EVR_reg_map__gp0_params__GP0_ADDR_W;
-localparam EVR_reg_map__gp0_params_e        GP0_DATA_W_EVAL   = EVR_reg_map__gp0_params__GP0_DATA_W;
-localparam EVR_reg_map__mmr_params_e        MMR_ADDR_W_EVAL   = EVR_reg_map__mmr_params__MMR_BLOCK_ADDR_W;
-localparam EVR_reg_map__mmr_params_e        MMR_DATA_W_EVAL   = EVR_reg_map__mmr_params__MMR_BLOCK_DATA_W;
-localparam EVR_reg_map__mmr_dev_params_e    MMR_DEV_CNT2_EVAL = EVR_reg_map__mmr_dev_params__MMR_DEV_CNT;
-
-localparam GP0_ADDR_W     = unsigned'(GP0_ADDR_W_EVAL);
-localparam GP0_DATA_W     = unsigned'(GP0_DATA_W_EVAL);
-localparam MMR_ADDR_W     = unsigned'(MMR_ADDR_W_EVAL);
-localparam MMR_DATA_W     = unsigned'(MMR_DATA_W_EVAL);
-localparam MMR_DEV_CNT2   = unsigned'(MMR_DEV_CNT2_EVAL);
-
-typedef logic [GP0_ADDR_W-1:0] gp0_addr_t;
-typedef logic [GP0_DATA_W-1:0] gp0_data_t;
-typedef logic [MMR_ADDR_W-1:0] mmr_addr_t;
-typedef logic [MMR_DATA_W-1:0] mmr_data_t;
-endpackage
-
 package EVR_axi_params;
 
 import EVR_reg_map_pkg::*;
