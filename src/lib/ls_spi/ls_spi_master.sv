69d04ddfb69fcc504da17f9c31aeaea6722b6567
Arsenii2013 <a.nepein@g.nsu.ru>
2025-10-20 15:24:04 +0700

    Change hand written low speed spi to spi core

--- src/lib/ls_spi/ls_spi_master.sv
+++ src/lib/ls_spi/ls_spi_master.sv
@@ -1,206 +1,195 @@
+//******************************************************************************
+//*
+//*      SPI 
+//*
+//*      Version 2.0
+//*
+//*      Copyright (c) 2004-2005, Harry E. Zhurov
+//*
+//*      $Revision: 77 $
+//*      $Date:: 2010-08-25 12:19:39 #$
+//*
+//------------------------------------------------------------------------------
+
+`timescale 1ns / 1ps
+
+
 module ls_spi_master #(
-    parameter unsigned  DATA_W      = 32,
-    parameter unsigned  SPI_W       = 1,
-    parameter unsigned  PRESCALER   = 2,
-    parameter unsigned  FRONT       = "rise",
-    parameter unsigned  FIRST       = "LSB",
-    parameter string    ALWAYS_SCK  = "no"
-)(
-    input  logic                clk,
-    input  logic                reset,
+                parameter PRESCALER   = 100, 
+                parameter CPOL        = 0,
+                parameter DATA_WIDTH  = 8
+              )
+(
+    //----------------------------------
+    //
+    //    External interface
+    //
+    output logic  SCK,       // serial clock
+    output logic  MOSI,      // serial data output
+    input  wire   MISO,      // serial data input
+       
+    //----------------------------------
+    //
+    //    Internal interface
+    //
+    input  wire                     clk,       // system clock
+    input  wire                     rst,       // system reset
+                                
+    input  wire                     enable,    // enable processing
+    input  wire                     load,      // load data for shift out
+    input  wire [DATA_WIDTH-1:0]    data_in,   // data to write out to output shift register
+    output logic [DATA_WIDTH-1:0]   data_out,  // received data from serial input
+    
+    output logic                    busy,      // data register not empty or serial transfer is active
+    output logic                    dre,       // data register empty
+    output logic                    stc        // serial transfer is completed
 
-    input  logic [DATA_W-1: 0]  tx_data,
-    output logic [DATA_W-1: 0]  rx_data,
-
-    input  logic                tx_load,
-    output logic                tx_empty,
-    output logic                rx_full,
-    output logic                busy,
-
-    output logic                SCK,
-    input  logic [SPI_W -1: 0]  MISO,
-    output logic [SPI_W -1: 0]  MOSI,
-    output logic                CS_N
 );
-    if (DATA_W % SPI_W != 0) begin
-        $error("DATA_W must be a multiple of SPI_W");
-    end
-    if (PRESCALER <= 1) begin
-        $error("PRESCALER must be greater than 1");
-    end
-    if (PRESCALER % 2 != 0) begin
-        $error("PRESCALER must be even");
-    end
-    if (FRONT != "rise" && FRONT != "fall") begin
-        $error("FRONT allowed values are rise or fall");
-    end
-    if (ALWAYS_SCK != "no" && ALWAYS_SCK != "yes") begin
-        $error("ALWAYS_SCK allowed values are no or yes");
-    end
-    localparam NIBBLE_COUNT = DATA_W / SPI_W;
 
-    typedef logic [DATA_W               -1: 0] data_t;
-    typedef logic [SPI_W                -1: 0] spi_t;
-    typedef logic [$clog2(PRESCALER)    -1: 0] prescaler_t;
-    typedef logic [$clog2(NIBBLE_COUNT)   : 0] nibble_cnt_t;
-    typedef enum logic [1:0]{
-        IDLE,
-        START,
-        TRANSFER,
-        FINISH
-    } state_t;
+import common::*;
 
-    state_t state = IDLE;
-    logic finish = 0;
+//------------------------------------------------------------------------------
+//
+//    Objects
+//
 
-// Free run SCK prescaler
-    prescaler_t psc_cnt = '0;
-    logic SCK_iternal = 0;
-    always_ff @(posedge clk) begin
+localparam CPHA       = 1;
 
-        if(psc_cnt == PRESCALER) begin
-            psc_cnt <= '0;
-        end else begin
-            psc_cnt <= psc_cnt + 1;
+localparam SCK_PERIOD = PRESCALER; // in clk ticks
+localparam SCK_DRIVE  = 1;
+localparam SCK_SAMPLE = SCK_PERIOD/2 + 1;
+localparam SCK_STOP   = CPHA ? SCK_DRIVE-1 : SCK_SAMPLE-1;
+
+bit [$clog2(SCK_PERIOD)-1:0] SCK_cnt;
+bit [        DATA_WIDTH-1:0] bufreg;
+bit                         bufempty;
+bit [        DATA_WIDTH-1:0] shiftreg;
+bit [                   1:0] miso_sync;
+bit [ $bits(DATA_WIDTH)-1:0] bitcnt;
+bit                         ready;
+
+
+//------------------------------------------------------------------------------
+//
+//    Logic
+//
+
+//assign SCK = SCK_cnt < SCK_PERIOD/2 ? CPOL : ~CPOL;
+
+//------------------------------------------------------------------------------
+//
+//    SPI clock generation logic
+//
+always_ff @(posedge clk, posedge rst) begin
+    if(rst) begin
+        SCK_cnt <= SCK_STOP;
+        SCK     <= CPOL;
+    end
+    else begin
+
+        if( (!bufempty && ready && enable) || bitcnt || SCK_cnt != SCK_STOP ) begin
+            SCK_cnt <= SCK_cnt + 1;
+            if(SCK_cnt == SCK_PERIOD-1) begin
+                SCK_cnt <= 0;
+            end
         end
-        if(psc_cnt == 0) begin
-            SCK_iternal <= 1;
+
+//      if( !busy ) begin
+//          SCK_cnt <= SCK_SAMPLE;
+//      end
+
+
+        if(CPHA) begin
+            SCK <= SCK_cnt >= SCK_DRIVE && SCK_cnt < SCK_SAMPLE ? ~CPOL : CPOL; 
         end
-        if(psc_cnt == PRESCALER / 2) begin
-            SCK_iternal <= 0;
+        else begin
+            SCK <= SCK_cnt >= SCK_SAMPLE ? CPOL : ~CPOL; 
         end
+
     end
+end
 
-// Posedge and negedge of free run SCK
-    logic SCK_posedge;
-    logic SCK_negedge;
-    logic SCK_iternal_prev;
-    always_ff @(posedge clk) SCK_iternal_prev <= SCK_iternal;
-    assign SCK_posedge = SCK_iternal  && !SCK_iternal_prev;
-    assign SCK_negedge = !SCK_iternal && SCK_iternal_prev;
+//------------------------------------------------------------------------------
+//
+//    Load and Shift logic
+//
+always_ff @(posedge clk) begin
+    if(rst) begin
+        bufreg   <= 0;
+        bufempty <= 1;
+        shiftreg <= 0;
+        bitcnt   <= 0; 
+        ready    <= 0;
 
-    logic drive_front, sample_front;
-    generate
-    if(FRONT == "rise") begin
-        assign drive_front = SCK_negedge;
-        assign sample_front = SCK_posedge;
-    end else if(FRONT == "fall") begin
-        assign drive_front = SCK_posedge;
-        assign sample_front = SCK_negedge;
+        data_out <= 0;
     end
-    endgenerate
+    else begin
 
-
-// SCK gate
-    logic SCK_gate;
-    generate
-    if(ALWAYS_SCK == "yes") begin
-        assign SCK_gate = 1;
-    end else begin
-        assign SCK_gate = state == TRANSFER || state == FINISH;
-    end
-    endgenerate
-
-    assign SCK = SCK_gate ? SCK_iternal : 0;
-
-// State logic
-    logic tx_load_reg = 0;
-    always_ff @(posedge clk) begin
-        if(reset) begin
-            tx_load_reg <= 0;
-        end else if(tx_load) begin
-            tx_load_reg <= 1;
-        end else if(state == START) begin
-            tx_load_reg <= 0;
+        if(!ready) begin
+            ready <= load;     // start processing
         end
-    end
 
-    assign busy     = state != IDLE;
-    assign CS_N     = state != IDLE;
-    always_ff @(posedge clk) begin
-        if(reset) begin
-            state <= IDLE;
-        end else begin
-            case(state)
-            IDLE        : state <= tx_load_reg ? START     : IDLE;
-            START       : state <= drive_front ? TRANSFER  : START;
-            TRANSFER    : state <= tx_empty    ? FINISH    : TRANSFER;
-            FINISH      : state <= drive_front ? 
-                                  (tx_load_reg ? START     : IDLE)
-                                                           : FINISH;
-            endcase
+        miso_sync[0] <= MISO;
+        miso_sync[1] <= miso_sync[0];
+
+        if(load) begin
+            bufreg   <= data_in;
+            bufempty <= 0;
         end
-    end
 
-// TX logic 
-    data_t tx_data_buf = 0;
-    always_ff @(posedge clk) begin
-        if(tx_load) begin
-            tx_data_buf <= tx_data;
+        case(SCK_cnt)
+            //-------------------------------------
+            SCK_DRIVE: begin 
+                MOSI                     <= shiftreg[DATA_WIDTH-1];
+                shiftreg[DATA_WIDTH-1:1] <= shiftreg[DATA_WIDTH-2:0];
+            end
+            //-------------------------------------
+            SCK_SAMPLE: begin 
+                //shiftreg[0] <= miso_sync[1];
+                shiftreg[0] <= MISO;
+            end
+            //-------------------------------------
+        endcase
+
+        //if( CPHA ? SCK_cnt == SCK_SAMPLE : SCK_cnt == SCK_DRIVE ) begin
+        if( SCK_cnt == SCK_STOP ) begin
+            if(bitcnt) begin
+                bitcnt <= bitcnt - 1;
+            end
         end
-    end
 
-    nibble_cnt_t tx_cnt = 0;
-    assign MOSI = spi_t'(tx_data_buf);
-
-    always_ff @(posedge clk) begin
-        if(tx_load || reset) begin
-            tx_cnt      <= 0;
-        end else if(drive_front && state == TRANSFER) begin
-            tx_cnt      <= tx_cnt + 1;
-            tx_data_buf <= {spi_t'(0), tx_data_buf[DATA_W-1: SPI_W]};
-        end
-    end
-
-    logic tx_empty_reg = 1;
-    assign tx_empty = tx_empty_reg;
-    always_ff @(posedge clk) begin
-        if(reset) begin
-            tx_empty_reg <= 1;
-        end else begin
-            if(tx_load)
-                tx_empty_reg <= 0;
-            else if(tx_cnt == NIBBLE_COUNT - 1)
-                tx_empty_reg <= 1;
-        end
-    end
-
-// RX logic 
-    logic rx_full_reg_0 = 0, rx_full_reg_1 = 0, rx_full_reg_2 = 0;
-    always_ff @(posedge clk) rx_full_reg_1 <= rx_full_reg_0;
-    always_ff @(posedge clk) rx_full_reg_2 <= rx_full_reg_1;
-    assign rx_full = rx_full_reg_2;
-
-    always_ff @(posedge clk) begin
-        if(reset) begin
-            rx_full_reg_0 <= 0;
-        end else begin
-            if(tx_load) begin
-                rx_full_reg_0 <= 0;
-                rx_full_reg_1 <= 0;
-                rx_full_reg_2 <= 0;
-            end else if(tx_empty && sample_front) begin
-                rx_full_reg_0 <= 1;
-                rx_full_reg_1 <= rx_full_reg_0;
-                rx_full_reg_2 <= rx_full_reg_1;
-            end else begin
-                rx_full_reg_0 <= rx_full_reg_0;
-                rx_full_reg_1 <= rx_full_reg_0;
-                rx_full_reg_2 <= rx_full_reg_1;
+        //if( !bitcnt && ( CPHA ? SCK_cnt == SCK_SAMPLE : SCK_cnt == SCK_DRIVE) ) begin
+        if( !bitcnt && SCK_cnt == SCK_STOP ) begin
+            data_out <= shiftreg;
+            if(ready && enable) begin
+                if( !bufempty ) begin
+                    shiftreg <= bufreg;
+                    bufempty <= 1;
+                    bitcnt   <= DATA_WIDTH-1;
+                end
             end
         end
     end
+end
 
-    data_t rx_data_buf;
-    always_ff @(posedge clk) begin
-        if(!rx_full_reg_1 && rx_full_reg_0) begin
-            rx_data <= rx_data_buf;
-        end
-    end
+assign dre  = bufempty;
+assign busy = bitcnt || !bufempty || SCK_cnt != SCK_STOP;
 
-    always_ff @(posedge clk) begin
-        if(sample_front) begin
-            rx_data_buf <= {MISO, rx_data_buf[DATA_W-1: SPI_W]};
-        end
-    end
-endmodule
+//------------------------------------------------------------------------------
+pf
+#(
+    .WIDTH(1),
+    .POR("OFF")
+)
+STC
+(
+    .clk ( clk    ),
+    .in  ( !bitcnt && SCK_cnt == SCK_STOP  ),
+    .out ( stc    )
+);
+//------------------------------------------------------------------------------
+
+endmodule 
+
+//------------------------------------------------------------------------------
+
