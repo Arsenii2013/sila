9c2449957b5d3c13fe019dda2a6c55309d3a531a
Arsenii2013 <a.nepein@g.nsu.ru>
2025-11-12 12:26:32 +0700

    Fix race condition in EVR i2c mux test

--- src/syn/PS_wrapper.sv
+++ src/syn/PS_wrapper.sv
@@ -347,6 +347,7 @@ module PS_wrapper_sv #(
 
     if(SIM_DEVICE == "EVR") begin
         initial begin
+            #1;
             forever begin
                 for(int i = 0; i < 4; i ++) begin
                     generic_generator.write(`BASE_FROM_NUMBER(EVR_axi_params::I2C_MUX) + 'h4, i);
